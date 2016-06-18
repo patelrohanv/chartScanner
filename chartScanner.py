a13046228135de835d119809882c2e7cb48b0d17
patelrohanv <patelrohanv@gmail.com>
2016-06-18 13:27:53 -0400

    1st commit

---
+++ chartScanner.py
@@ -0,0 +1,20 @@
+#import library
+import openpyxl
+#prompt for excel file and open it
+exfile = input("Enter the excel file you wish to open: ")
+wb = openpyxl.load_workbook(exfile)
+wb.get_sheet_names()
+#prompt for sheet
+page = input("Which sheet do you wish to work with: ")
+sheet = wb.get_sheet_by_name(page)
+#save value for highest row and column, will be used later for loops
+maxRow = sheet.max_row
+#print ("Max Row: ", maxRow)
+maxCol = sheet.max_column
+#print ("Max Column: ", maxCol)
+
+for i in range (2,maxRow):
+	if "blood" in sheet.cell(row=i, column = 2).value:
+		#print (i);
+		print ("found")
+
