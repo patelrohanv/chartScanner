8777ca4604a325f2bce7612f3018432f4b3b9954
patelrohanv <patelrohanv@gmail.com>
2016-06-27 10:39:37 -0400

    need to test

--- chartScanner.py
+++ chartScanner.py
@@ -8,13 +8,16 @@ wb.get_sheet_names()
 page = input("Which sheet do you wish to work with: ")
 sheet = wb.get_sheet_by_name(page)
 #save value for highest row and column, will be used later for loops
+words = input("What words do you want to search for: ")
+words.split()
 maxRow = sheet.max_row
 #print ("Max Row: ", maxRow)
 maxCol = sheet.max_column
 #print ("Max Column: ", maxCol)
 
 for i in range (2,maxRow):
-	if "blood" in sheet.cell(row=i, column = 2).value:
-		#print (i);
-		print ("found")
+	for word in words:
+		if word in sheet.cell(row=i, column = 2).value:
+			#print (i);
+			print ("found")
 
